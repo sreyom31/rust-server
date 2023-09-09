f5b81138354aeddc4dcc92ef4735d04812f035f8
sreyom31 <sreyom31@gmail.com>
2023-09-09 17:17:19 +0530

    :zap: using random port for tests.
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
   Ok(server)
