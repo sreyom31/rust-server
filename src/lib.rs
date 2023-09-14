b3a87dba15411b8b7766fbe872257e11ddc65203
sreyom31 <sreyom31@gmail.com>
2023-09-14 09:16:03 +0530

    :wrench: before use of extractors.
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
