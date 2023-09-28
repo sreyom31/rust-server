79cb893a48650710a61cc4bed188c6f2e8b34688
sreyom31 <sreyom31@gmail.com>
2023-09-29 01:20:05 +0530

    :zap: creating new db for test.
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
@@ -5,7 +6,10 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
