1cc676219e2b79bf807d9ced6524167070a96656
sreyom31 <sreyom31@gmail.com>
2023-09-29 23:39:58 +0530

    :hammer: instrumentation and different module for telemetry.
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
-use env_logger::Env;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use chrono::Utc; 
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -19,7 +20,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     );
     let _request_span_guard = request_span.enter();
 
-    tracing::info!("request_id {} - Saving new subscriber details in the database.", request_id);
+    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -30,15 +31,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            tracing::info!("request_id {} - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
