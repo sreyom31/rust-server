57b77b58604b5aa7022b378ec7a03f17776296c3
sreyom31 <sreyom31@gmail.com>
2023-10-05 19:14:02 +0530

    :zap: refactor: new function send_confirmation_mail
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -14,7 +14,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-    
+
     let response = app.post_subscriptions(body.into()).await;
     // Assert
     assert_eq!(200, response.status().as_u16());
@@ -89,4 +89,37 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     app.post_subscriptions(body.into()).await;
     // Assert
     // Mock asserts on drop
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        // We are not setting an expectation here anymore // The test is focused on another aspect of the app // behaviour.
+        .mount(&app.email_server)
+        .await;
+    // Act
+    app.post_subscriptions(body.into()).await;
+    // Assert
+    // Get the first intercepted request
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    // Parse the body as JSON, starting from raw bytes
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+    
+    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(&body["TextBody"].as_str().unwrap()); // The two links should be identical
+    assert_eq!(html_link, text_link);
 }
